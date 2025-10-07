e348a258409327dc713f3787e1a3a442a7dfbddf
nathaniams <nathaniams@gmail.com>
2025-10-07 09:23:30 -0400

    Page added for project

---
+++ PracticeCode.R
@@ -0,0 +1,17 @@
+require(graphics)
+mosaicplot(Titanic, main = "Survival on the Titanic")
+apply(Titanic, c(3,4), sum)
+view(Titanic)
+summary(Titanic)
+head(Titanic)
+Titanic
+
+starwars
+
+us_rent_income
+library(tidyr)
+us_rent_income
+
+penguins
+View(penguins)
+summary(penguins)
